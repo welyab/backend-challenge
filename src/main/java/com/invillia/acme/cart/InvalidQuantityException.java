6ebc8aceb6a03ab95e58341f463a35cfccba6106
Welyab Paula <welyab.exe@gmail.com>
2019-03-22 20:07:40 -0300

    Javadoc the class.

--- src/main/java/com/invillia/acme/cart/InvalidQuantityException.java
+++ src/main/java/com/invillia/acme/cart/InvalidQuantityException.java
@@ -1,18 +1,30 @@
 package com.invillia.acme.cart;
 
+/**
+ * Indicates that a invalid quantity was attemped to be inserted in the cart.
+ * 
+ * @author Welyab Paula
+ */
 public class InvalidQuantityException extends CartException {
 
+	@SuppressWarnings("javadoc")
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings("javadoc")
 	public InvalidQuantityException() {
 	}
 
+	@SuppressWarnings("javadoc")
 	public InvalidQuantityException(String message) {
 		super(message);
 	}
 
+	@SuppressWarnings("javadoc")
 	public InvalidQuantityException(Throwable cause) {
 		super(cause);
 	}
 
+	@SuppressWarnings("javadoc")
 	public InvalidQuantityException(String message, Throwable cause) {
 		super(message, cause);
 	}
