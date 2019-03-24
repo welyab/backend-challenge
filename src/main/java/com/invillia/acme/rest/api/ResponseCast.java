65b6078ed3521e445c0c7a75f60e939ebbe20fe4
Welyab Paula <welyab.exe@gmail.com>
2019-03-24 01:50:29 -0300

    Javadoc the class.

--- src/main/java/com/invillia/acme/rest/api/ResponseCast.java
+++ src/main/java/com/invillia/acme/rest/api/ResponseCast.java
@@ -2,8 +2,25 @@ package com.invillia.acme.rest.api;
 
 import org.springframework.http.ResponseEntity;
 
+/**
+ * Auxiliary class for performs unsafe casts in <code>ResponseEntity</code>
+ * objects.
+ * 
+ * @author Welyab Paula
+ */
 public class ResponseCast {
 
+	@SuppressWarnings("javadoc")
+	private ResponseCast() {
+	}
+
+	/**
+	 * Performs a unsafe cast in the given parameter.
+	 * 
+	 * @param response The object to be casted.
+	 * 
+	 * @return The same reference passed in the <code>response</code> parameter.
+	 */
 	public static <E> ResponseEntity<E> cast(ResponseEntity<?> response) {
 		@SuppressWarnings("unchecked")
 		ResponseEntity<E> e = (ResponseEntity<E>) response;
